95206fde08bd41c3a329982f80f86331db4883a6
geetayadav23 <161703030+geetayadav23@users.noreply.github.com>
2024-03-07 18:47:19 +0530

    Add files via upload
    *

---
+++ pattern1.js
@@ -0,0 +1,14 @@
+const input = require("readline-sync")
+let n = input.questionInt("enter the number of rows")
+i=1;
+s=""
+while(i<=n){
+    j=1;
+    while(j<=i){
+        s+=("*")
+        j++;;
+    }
+    s+=("\n")
+    i++;
+}
+console.log(s)
