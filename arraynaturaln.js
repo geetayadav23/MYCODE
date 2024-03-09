8977b246960d6fdf11bb38af25a7c3dc95c30b07
geetayadav23 <161703030+geetayadav23@users.noreply.github.com>
2024-03-09 14:20:38 +0530

    array

---
+++ arraynaturaln.js
@@ -0,0 +1,15 @@
+
+const input = require("readline-sync");
+let n = input.questionInt("enter the number: ");
+let array=[]
+i=0;
+b=1
+while (i<n) {
+   array[i]=b
+   i++;
+   b++;
+}
+
+
+    console.log(array);
+    
