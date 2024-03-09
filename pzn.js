dab0cda61fb4d6012b5419e416778511c663082a
geetayadav23 <161703030+geetayadav23@users.noreply.github.com>
2024-03-09 11:26:38 +0530

    positive,zero,negative- condition

---
+++ pzn.js
@@ -0,0 +1,16 @@
+const  input = require('readline-sync')
+let num= input.question();
+if(num>0){
+    console.log("positive")
+
+}
+else if (num==0){
+    console.log("zero")
+
+}
+else{
+    console.log("negative")
+}
+
+
+
